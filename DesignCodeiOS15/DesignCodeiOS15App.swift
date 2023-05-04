93c14ce1c51dd99bc0bd4cb4324b81a6b0754ff0
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-05-04 20:00:21 +0200

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Alexander Sobolev on 4.5.23..
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
