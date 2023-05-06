f08cdaf8fb9ee51053d937c9ced3cc210bf02df1
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-05-06 22:05:37 +0200

    Refactor

--- DesignCodeiOS15/AccountView.swift
+++ DesignCodeiOS15/AccountView.swift
@@ -9,93 +9,16 @@ import SwiftUI
 
 struct AccountView: View {
     @State var isDeleted = false
+    @State var isPinned  = false
+    
     var body: some View {
         NavigationView {
             List {
-                VStack(spacing: 8) {
-                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
-                        .symbolVariant(.circle.fill)
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(
-                            Image(systemName: "hexagon")
-                                .symbolVariant(.fill)
-                                .foregroundColor(.blue)
-                                .font(.system(size: 200))
-                                .offset(x: -50, y: -100)
-                    )
-                    Text("SOBOLEV")
-                        .font(.title.weight(.semibold))
-                        .foregroundStyle(.linearGradient(colors: [.blue, .blue.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    HStack {
-                        Image(systemName: "location")
-                            .imageScale(.small)
-                            .foregroundColor(.blue)
-                        Text("Canada")
-                            .foregroundStyle(.linearGradient(colors: [.blue, .blue.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                Section {
-                    NavigationLink(destination: ContentView()) {
-                        Label("Settings", systemImage: "gear")
-                    }
-                    NavigationLink {
-                        ContentView()
-                    } label: {
-                        Label("Billing", systemImage: "creditcard")
-                    }
-                    NavigationLink {
-                        ContentView()
-                    } label: {
-                        Label("Money", systemImage: "questionmark")
-                    }
-                }
-                .foregroundColor(.primary)
-                .listRowSeparatorTint(.blue)
-                .listRowSeparator(.hidden)
+                profile
                 
-                Section {
-                    if !isDeleted {
-                        Link(destination: URL(string: "https://apple.com")!) {
-                            HStack {
-                                Label("Website", systemImage: "house")
-                                Spacer()
-                                Image(systemName: "link")
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                        .swipeActions(edge: .leading, allowsFullSwipe: false) {
-                            Button {
-                                isDeleted = true
-                            } label: {
-                                Label("Delete", systemImage: "trash")
-                            }
-                            .tint(.red)
-                            Button {
-                                //
-                            } label: {
-                                Label("Pin", systemImage: "pin")
-                            }
-                            .tint(.yellow)
-
-
-                        }
-                    }
-                    Link(destination: URL(string: "https://youtube.com")!) {
-                        HStack {
-                            Label("YouTube", systemImage: "tv")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                }
-                .foregroundColor(.primary)
-                .listRowSeparator(.hidden)
+                menu
+                
+                links
             }
             .listStyle(.sidebar)
             .navigationTitle("Account")
@@ -106,5 +29,108 @@ struct AccountView: View {
 struct AccountView_Previews: PreviewProvider {
     static var previews: some View {
         AccountView()
+    }
+}
+
+extension AccountView {
+    var profile: some View {
+        VStack(spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    Image(systemName: "hexagon")
+                        .symbolVariant(.fill)
+                        .foregroundColor(.blue)
+                        .font(.system(size: 200))
+                        .offset(x: -50, y: -100)
+            )
+            Text("SOBOLEV")
+                .font(.title.weight(.semibold))
+                .foregroundStyle(.linearGradient(colors: [.blue, .blue.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.small)
+                    .foregroundColor(.blue)
+                Text("Canada")
+                    .foregroundStyle(.linearGradient(colors: [.blue, .blue.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+    
+    var menu: some View {
+        Section {
+            NavigationLink(destination: ContentView()) {
+                Label("Settings", systemImage: "gear")
+            }
+            NavigationLink {
+                ContentView()
+            } label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            NavigationLink {
+                ContentView()
+            } label: {
+                Label("Money", systemImage: "questionmark")
+            }
+        }
+        .foregroundColor(.primary)
+        .listRowSeparatorTint(.blue)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View {
+        Section {
+            if !isDeleted {
+                Link(destination: URL(string: "https://apple.com")!) {
+                    HStack {
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .swipeActions(edge: .leading, allowsFullSwipe: false) {
+                    Button {
+                        isDeleted = true
+                    } label: {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red)
+                    pinButton
+                }
+            }
+            Link(destination: URL(string: "https://youtube.com")!) {
+                HStack {
+                    Label("YouTube", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .swipeActions {
+                pinButton
+            }
+        }
+        .foregroundColor(.primary)
+        .listRowSeparator(.hidden)
+    }
+    
+    var pinButton: some View {
+        Button {
+            isPinned.toggle()
+        } label: {
+            if isPinned {
+                Label("Unpin", systemImage: "pin.slash")
+            } else {
+                Label("Pin", systemImage: "pin")
+            }
+        }
+        .tint(isPinned ? .gray : .yellow)
     }
 }
