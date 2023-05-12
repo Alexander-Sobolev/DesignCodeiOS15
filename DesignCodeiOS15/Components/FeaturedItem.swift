45ddf35749fbb76cc03f80e577c47aecca9675ee
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-05-12 20:03:13 +0200

    TabViewPagination

--- DesignCodeiOS15/Components/FeaturedItem.swift
+++ DesignCodeiOS15/Components/FeaturedItem.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct FeaturedItem: View {
+    var course: Course = courses[0]
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
             Spacer()
-            Image("Logo 2")
+            Image(course.logo)
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26, height: 26)
@@ -19,15 +21,15 @@ struct FeaturedItem: View {
                 .padding(9)
                 .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
                 .strokeStyle(cornerRadius: 16)
-            Text("SwiftUI for iOS 15")
+            Text(course.title)
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-            Text("20 sections - 3 hours".uppercased())
+            Text(course.subtitle.uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
                 .foregroundStyle(.secondary)
-            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+            Text(course.text)
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
@@ -41,12 +43,8 @@ struct FeaturedItem: View {
         .shadow(color: Color("Shadow").opacity(0.5), radius: 10, x: 0, y: 10)
         .strokeStyle()
         .padding(.horizontal, 20)
-        .background(
-            Image("Blob 1")
-                .offset(x: 250, y: -100)
-        )
         .overlay(
-            Image("Illustration 5")
+            Image(course.image)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(height: 230)
--- DesignCodeiOS15/Views/HomeView.swift
+++ DesignCodeiOS15/Views/HomeView.swift
@@ -11,39 +11,65 @@ struct HomeView: View {
     @State var hasScrolled = false
     
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            GeometryReader { proxy in
-//                Text("\(proxy.frame(in: .named("scroll")).minY)")
-                Color.clear.preference(
-                    key: ScrollPreferenceKey.self,
-                    value: proxy.frame(in: .named("scroll")).minY
-                )
+        ZStack {
+            Color("Background")
+                .ignoresSafeArea()
+            
+            ScrollView(showsIndicators: false) {
+                scrollDetection
+                
+                featured
+                
+                Color.clear.frame(height: 1000)
             }
-            .frame(height: 0)
-            FeaturedItem()
-            Color.clear.frame(height: 1000)
-        }
-        .coordinateSpace(name: "scroll")
-        .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
-            withAnimation(.easeInOut) {
-                if value < 0 {
-                    hasScrolled = true
-                } else {
-                    hasScrolled = false
+            .coordinateSpace(name: "scroll")
+            .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
+                withAnimation(.easeInOut) {
+                    if value < 0 {
+                        hasScrolled = true
+                    } else {
+                        hasScrolled = false
+                    }
                 }
-            }
-        })
-        .safeAreaInset(edge: .top, content: {
-            Color.clear.frame(height: 70)
-        })
-        .overlay(
-            NavigationBar(hasScrolled: $hasScrolled, title: "Featured")
+            })
+            .safeAreaInset(edge: .top, content: {
+                Color.clear.frame(height: 70)
+            })
+            .overlay(
+                NavigationBar(hasScrolled: $hasScrolled, title: "Featured")
         )
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+extension HomeView {
+    var scrollDetection: some View {
+        GeometryReader { proxy in
+            Color.clear.preference(
+                key: ScrollPreferenceKey.self,
+                value: proxy.frame(in: .named("scroll")).minY
+            )
+        }
+        .frame(height: 0)
+    }
+    
+    var featured: some View {
+        TabView {
+            ForEach(courses) { item in
+                FeaturedItem(course: item)
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(height: 430)
+        .background(
+            Image("Blob 1")
+                .offset(x: 250, y: -100)
+        )
     }
 }
